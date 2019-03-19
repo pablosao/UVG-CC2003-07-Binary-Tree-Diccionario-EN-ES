08abefb47b6c6bbcb84de11aa1ae31d4ded8324d
Pablo Sao <pablo_sao@outlook.com>
2019-03-19 15:44:02 -0600

    creando clase para manejo de data y completando dinamica del menú

--- TraductorArbol/src/TraductorArbol.java
+++ TraductorArbol/src/TraductorArbol.java
@@ -31,11 +31,59 @@ public class TraductorArbol {
             switch (opcion){
                 case 1:
                     //Llamamos metodo para leer archivos y cargar al arbol.
-
+                    getDiccionario();
+                    break;
                 case 2:
                     System.exit(0);
             }
         }
 
     }
+
+    private static void getDiccionario(){
+
+
+        try{
+            System.out.print("\nIngrese el PATH del archivo para el diccionaro: ");
+            String path = Keyboard.readString();
+            if(DataManager.getExists(path)){
+
+                //Proceso para cargar diccionario al arbol binario
+
+                //Nos trasladamos al segundo menú
+                getTraduccion();
+            }
+            else{
+                System.out.println(String.format("\n\t\tEl archivo no ha se encontro.\n\t\tRuta: %s",path));
+            }
+        }
+        catch (Exception e){
+            System.out.println("Existe un inconveniente al cargar el diccionario.\nError: " + e.toString());
+        }
+    }
+
+    public static void  getTraduccion(){
+        int opcion = 0;
+        boolean control = true;
+
+        while(control){
+            System.out.println(SEGUNDO_MENU);
+            System.out.print("\nIngrese el número de opción: ");
+            opcion = Keyboard.readInt();
+
+            switch (opcion){
+                case 1:
+
+                    break;
+                case 2:
+                    control = false;
+                    break;
+                case 3:
+                    System.exit(0);
+            }
+        }
+
+        getDiccionario();
+    }
+
 }
